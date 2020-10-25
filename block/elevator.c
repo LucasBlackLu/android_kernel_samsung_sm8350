34f7d3a66afb441cb115df2e3dd33f16b2c68d6e
Yaroslav Furman <yaro330@gmail.com>
2020-10-25 17:08:54 -0400

    block: elevator: forbid init from changing I/O scheduler
    Userspace sets bfq when booting is done, we do not want this to happen.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Change-Id: I0013ab117bb3adbadcb693117423a4a8e032f7ef
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- block/elevator.c
+++ block/elevator.c
@@ -124,6 +124,10 @@ static struct elevator_type *elevator_find(const char *name,
 {
 	struct elevator_type *e;
 
+	/* Forbid init from changing I/O scheduler by default */
+	if (!strncmp(current->comm, "init", sizeof("init")))
+		return NULL;
+
 	list_for_each_entry(e, &elv_list, list) {
 		if (elevator_match(e, name, required_features))
 			return e;
