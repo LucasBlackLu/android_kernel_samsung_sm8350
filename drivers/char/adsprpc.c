166fb72329e95f6b35353b6701c8a8709cacde3c
Divyanshu-Modi <divyan.m05@gmail.com>
2022-08-02 15:17:43 +0530

    drivers: disable debugfs file creation when !CONFIG_DEBUG_FS
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Change-Id: I19a91aaf3df6225fdd57ec6bc0771e4b0971bf55
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -276,8 +276,10 @@ enum fastrpc_msg_type {
 static int fastrpc_pdr_notifier_cb(struct notifier_block *nb,
 					unsigned long code,
 					void *data);
+#ifdef CONFIG_DEBUG_FS
 static struct dentry *debugfs_root;
 static struct dentry *debugfs_global_file;
+#endif
 
 static inline uint64_t buf_page_start(uint64_t buf)
 {
@@ -5481,13 +5483,16 @@ static int fastrpc_device_release(struct inode *inode, struct file *file)
 			dev_pm_qos_remove_request(&fl->dev_pm_qos_req[ii]);
 		}
 	}
+#ifdef CONFIG_DEBUG_FS
 	debugfs_remove(fl->debugfs_file);
+#endif
 	fastrpc_file_free(fl);
 	file->private_data = NULL;
 
 	return 0;
 }
 
+#ifdef CONFIG_DEBUG_FS
 static ssize_t fastrpc_debugfs_read(struct file *filp, char __user *buffer,
 					 size_t count, loff_t *position)
 {
@@ -5719,6 +5724,7 @@ static const struct file_operations debugfs_fops = {
 	.open = simple_open,
 	.read = fastrpc_debugfs_read,
 };
+#endif
 
 static int fastrpc_channel_open(struct fastrpc_file *fl)
 {
@@ -5876,6 +5882,7 @@ bail:
 	return err;
 }
 
+#ifdef CONFIG_DEBUG_FS
 static int fastrpc_set_process_info(struct fastrpc_file *fl)
 {
 	int err = 0, buf_size = 0;
@@ -5926,6 +5933,22 @@ static int fastrpc_set_process_info(struct fastrpc_file *fl)
 	}
 	return err;
 }
+#else
+static int fastrpc_set_process_info(struct fastrpc_file *fl)
+{
+	fl->tgid = current->tgid;
+
+	/*
+	 * Third-party apps don't have permission to open the fastrpc device, so
+	 * it is opened on their behalf by DSP HAL. This is detected by
+	 * comparing current PID with the one stored during device open.
+	 */
+	if (current->tgid != fl->tgid_open)
+		fl->untrusted_process = true;
+
+	return 0;
+}
+#endif
 
 static int fastrpc_get_info(struct fastrpc_file *fl, uint32_t *info)
 {
@@ -6751,6 +6774,7 @@ static int fastrpc_cb_probe(struct device *dev)
 	}
 
 	chan->sesscount++;
+#ifdef CONFIG_DEBUG_FS
 	if (debugfs_root && !debugfs_global_file) {
 		debugfs_global_file = debugfs_create_file("global", 0644,
 			debugfs_root, NULL, &debugfs_fops);
@@ -6760,6 +6784,7 @@ static int fastrpc_cb_probe(struct device *dev)
 			debugfs_global_file = NULL;
 		}
 	}
+#endif
 bail:
 	return err;
 }
@@ -7268,7 +7293,9 @@ static void __exit fastrpc_device_exit(void)
 	if (me->rpmsg_register == 1)
 		unregister_rpmsg_driver(&fastrpc_rpmsg_client);
 	kfree(me->gidlist.gids);
+#ifdef CONFIG_DEBUG_FS
 	debugfs_remove_recursive(debugfs_root);
+#endif
 }
 
 module_init(fastrpc_device_init);
