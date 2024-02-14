a1ae5fd44e06fced15b0748fee027dc9a4415fa1
glikched <florianvictorcristea@gmail.com>
2024-02-14 19:19:16 +0200

    sched/cass: adapt for msm-5.4
    In Linux kernel version 5.4.259, the cpus_allowed field in the struct task_struct has been replaced with cpus_ptr. The cpus_ptr is a pointer to the cpumask_t that represents the set of CPUs allowed for a task.

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -108,7 +108,7 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync)
 	 * idle_get_state().
 	 */
 	rcu_read_lock();
-	for_each_cpu_and(cpu, &p->cpus_allowed, cpu_active_mask) {
+	for_each_cpu_and(cpu, cpumask_of(task_cpu(p)), cpu_active_mask) {
 		/* Use the free candidate slot */
 		curr = &cands[cidx];
 		curr->cpu = cpu;
@@ -191,8 +191,8 @@ static int cass_select_task_rq_fair(struct task_struct *p, int prev_cpu,
 	 * first valid CPU since it's possible for certain types of tasks to run
 	 * on inactive CPUs.
 	 */
-	if (unlikely(!cpumask_intersects(&p->cpus_allowed, cpu_active_mask)))
-		return cpumask_first(&p->cpus_allowed);
+	if (unlikely(!cpumask_intersects(cpumask_of(task_cpu(p)), cpu_active_mask)))
+	    return cpumask_first(cpumask_of(task_cpu(p)));
 
 	/* cass_best_cpu() needs the task's utilization, so sync it up */
 	if (!(sd_flag & SD_BALANCE_FORK))
