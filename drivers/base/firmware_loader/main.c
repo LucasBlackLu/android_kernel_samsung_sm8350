5ab18e703f6ca13168b11a9c728df03089c30464
Georg Veichtlbauer <3273737+vware@users.noreply.github.com>
2024-05-01 10:13:42 +0000

    drivers: firmware: Add firmware directories for direct loading.
    * fix follwing eror
    kworker/u16:2: 1746]  subsys-pil-tz soc:qcom,ipa_fws: Direct firmware load for ipa_fws.mdt failed with error -2
    ueventd: firmware: attempted /etc/firmware/ipa_fws.mdt
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- drivers/base/firmware_loader/main.c
+++ drivers/base/firmware_loader/main.c
@@ -447,6 +447,7 @@ static char fw_path_para[256];
 static const char * const fw_path[] = {
 	fw_path_para,
 	"/vendor/firmware",
+	"/vendor/etc/firmware"
 	"/vendor/firmware_mnt/image",
 	"/firmware/image",
 	"/lib/firmware/updates/" UTS_RELEASE,
